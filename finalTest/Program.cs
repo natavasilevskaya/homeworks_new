c1bfc1443cdb0077ff21fd1807d50aa909d7562a
NataVasilevskaya <nata.vasilevskaya11@yandex.ru>
2023-03-16 00:08:37 +0300

    Task added

---
+++ finalTest/Program.cs
@@ -0,0 +1,30 @@
+﻿string[] array = new string[] { "verification", "o11", "27", "sdgwiuhlq;j", "sun", "213", "constant" };
+
+void PrintArray(string[] arr)
+{   
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+string[] CheckElements(string[] arr)
+{
+    int maxLength = 3;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= maxLength)
+        {
+            arr[i] = arr[i];
+        }
+        else arr[i] = "";
+    }
+    return arr;
+}
+
+Console.Write("Initial array: ");
+PrintArray(array);
+Console.Write("New array: ");
+PrintArray(CheckElements(array));
